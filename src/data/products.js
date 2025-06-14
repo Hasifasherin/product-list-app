dd4ddb17f910235ef681f2a0d520418ee6b54b74
Hasifa sherin CP <hasifasherincp@gmail.com>
2025-06-14 23:52:34 +0530

    add images

--- src/data/products.js
+++ src/data/products.js
@@ -2,23 +2,23 @@
 const products = [
   {
     id: 1,
-    name: "Smartphone",
-    price: 499,
-    image: "https://via.placeholder.com/150?text=Smartphone",
+    name: "OPPO Smartphone",
+    price: 10499,
+    image: "https://opsg-img-cdn-gl.heytapimg.com/epb/202412/19/AceLeaXtntKw1AZf.png",
     rating: 4.5
   },
   {
     id: 2,
-    name: "Laptop",
-    price: 899,
-    image: "https://via.placeholder.com/150?text=Laptop",
+    name: "HP Laptop",
+    price: 58899,
+    image: "https://cdn.mos.cms.futurecdn.net/pyL3b8cis5dcmUvgbe9ygV.jpg",
     rating: 4.2
   },
   {
     id: 3,
-    name: "Headphones",
-    price: 149,
-    image: "https://via.placeholder.com/150?text=Headphones",
+    name: "Samsung Headphones",
+    price: 4049,
+    image: "https://m.media-amazon.com/images/I/61ro69YFn1L.jpg",
     rating: 4.0
   }
 ];
